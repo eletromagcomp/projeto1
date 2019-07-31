1252739cd86b0c364de2e289a4fb9d452caf119f
Lord Momo <henriquetortura.ht@gmail.com>
2019-07-30 21:40:16 -0300

    Programa principal importa distribuição elíptica
    Programa principal agora importa função que distribui cargas dentro de uma elipse, isto é, cria a condição inicial para o problema base. Testem, façam sugestões e modificações a medida que necessário. Beijão.

--- projeto.py
+++ projeto.py
@@ -1,2 +1,13 @@
-Print("Hello World, yeah")
-Print("Now I'm master!")
+#Print("Hello World, yeah")
+#Print("Now I'm master!")
+
+import Elipse
+
+a = 100
+b = 50
+n = 1000
+
+Charges = []
+r = []
+
+Charges, r = Elipse.Criar_Condicao_Inicial(a,b,n)
