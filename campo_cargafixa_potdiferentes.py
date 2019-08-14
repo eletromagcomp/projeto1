ad359f43de1c03ad16e084c152863de05b5bdedb
bidufranjoao <49568329+bidufranjoao@users.noreply.github.com>
2019-08-14 19:39:35 -0300

    Update and rename campo_e_cargafixa.py to campo_cargafixa_potdiferentes.py

--- campo_cargafixa_potdiferentes.py
+++ campo_cargafixa_potdiferentes.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 #%% VARIÁVEIS
 def n(): #Número de cargas
-    return 300
+    return 100
 
 def a(): #Semi-eixo maior
     return 100
@@ -27,37 +27,59 @@ def initial_condition_np(a,b,n):
 
 
 # %% VARIAÇÃO DE POTENCIAL
-def Delta_pot(charges, charge_i, charge_i_new, i, condicao):
+def Delta_pot(charges, charge_i, charge_i_new, i, condicao, potencial):
     #pote: energia de interação da partícula i com a partícula k
     #total: guarda a soma das energias de interações
     #condicao: 1 - carga fixa ; 0 - sem carga fixa
+    #potencial: 0 - log; 1 - 1/r
     
     pote_old = np.zeros(1) #Tamanho 1 porquê entra só a coordenada inicial da partícula i
     pote_new = np.zeros(4) #Tamanho 4 porquê entra as coordenadas dos 4 movimentos possíveis da partícula i (norte, sul, leste, oeste)
     total_old = np.zeros(1)
-    total_new = np.zeros(4)
-    
-    if condicao == 1:
-        carga_fixa = [a+20,b+20]
-        total_old = -(len(charges)/2)*np.log(np.sqrt(np.sum((charge_i - carga_fixa)**2)))
-        total_new = -(len(charges)/2)*np.log(np.sqrt(np.sum((charge_i_new - carga_fixa)**2, axis = 1)))
+    total_new = np.zeros(4)        
         
-    for k in range(len(charges)):
-        if i!=k: #Não calculamos a energia de interação de uma partícula com ela mesma
-            charge_k = charges[k, :]
-            pote_old = np.log(np.sqrt(np.sum((charge_i - charge_k)**2)))
-            pote_new = np.log(np.sqrt(np.sum((charge_i_new - charge_k)**2, axis = 1)))
-            total_old = total_old- pote_old
-            total_new = total_new- pote_new
-    Delta_U = total_new - total_old
-    min_index = np.argmin(Delta_U) #Pega o primeiro índice do menor Delta_U
-    Delta_U_min = Delta_U[min_index] #Pega o valor do menor Delta_U
-    return Delta_U_min, min_index
+    if potencial == 0:
+        if condicao == 1:
+            carga_fixa = [a+20,b+20]
+            total_old = -(len(charges)/2)*np.log(np.sqrt(np.sum((charge_i - carga_fixa)**2)))
+            total_new = -(len(charges)/2)*np.log(np.sqrt(np.sum((charge_i_new - carga_fixa)**2, axis = 1)))
+            
+        for k in range(len(charges)):
+            if i!=k: #Não calculamos a energia de interação de uma partícula com ela mesma
+                charge_k = charges[k, :]
+                pote_old = np.log(np.sqrt(np.sum((charge_i - charge_k)**2)))
+                pote_new = np.log(np.sqrt(np.sum((charge_i_new - charge_k)**2, axis = 1)))
+                total_old = total_old- pote_old
+                total_new = total_new- pote_new
+        Delta_U = total_new - total_old
+        min_index = np.argmin(Delta_U) #Pega o primeiro índice do menor Delta_U
+        Delta_U_min = Delta_U[min_index] #Pega o valor do menor Delta_U
+        return Delta_U_min, min_index
+    
+    
+    if potencial == 1:
+        if condicao == 1:
+            carga_fixa = [a+20,b+20]
+            total_old = -(len(charges)/2)*(np.sqrt(np.sum((charge_i - carga_fixa)**2)))
+            total_new = -(len(charges)/2)*(np.sqrt(np.sum((charge_i_new - carga_fixa)**2, axis = 1)))
+            
+        for k in range(len(charges)):
+            if i!=k: #Não calculamos a energia de interação de uma partícula com ela mesma
+                charge_k = charges[k, :]
+                pote_old = np.sqrt(np.sum((charge_i - charge_k)**2))
+                pote_new = np.sqrt(np.sum((charge_i_new - charge_k)**2, axis = 1))
+                total_old = total_old- pote_old
+                total_new = total_new- pote_new
+        Delta_U = total_new - total_old
+        min_index = np.argmin(Delta_U) #Pega o primeiro índice do menor Delta_U
+        Delta_U_min = Delta_U[min_index] #Pega o valor do menor Delta_U
+        return Delta_U_min, min_index
+        
 
 
 
 #%% SIMULAÇÃO
-def simulate(a, b, n, charges, condicao):
+def simulate(a, b, n, charges, condicao, potencial):
     variacao = np.array([[1,0], [-1,0], [0,1], [0,-1]]) #Matriz de variação, contém os movimentos possíveis (norte, sul, leste, oeste)
     index_charges = np.arange(len(charges))
     flag = 0
@@ -81,7 +103,7 @@ def simulate(a, b, n, charges, condicao):
                 else:
                     charge_i_new[ I, :] = charge_i_new[I, :] - step*variacao[I, :] #Caso a partícula não satisfaça as duas condições, volto ela pra sua posição inicial
                     #Note que voltar pra posição inicial implica que o Delta_U para esse movimento vai ser 0 
-            Delta_U_min, min_index = Delta_pot(charges, charge_i, charge_i_new, i, condicao) #Vou pegar a menor variação energia dentre os movimentos e o índice que causa essa variação
+            Delta_U_min, min_index = Delta_pot(charges, charge_i, charge_i_new, i, condicao, potencial) #Vou pegar a menor variação energia dentre os movimentos e o índice que causa essa variação
             if Delta_U_min<0:
                 charges[i, :] = charge_i + step*variacao[min_index, :] #Movo a partícula pra a direção que causa maior diminuição da energia
             elif Delta_U_min==0:
@@ -104,7 +126,7 @@ def campo_eletrico(a,b,n,charges):
     print(1/campo,ponto)
     return campo
 #%% PLOT
-def plot(a, b, charges_0, charges):
+def plot(a, b, charges_0, charges, condicao):
     #Copia as posições iniciais em x e y
     x_0 = charges_0[:, 0]
     y_0 = charges_0[:, 1]
@@ -115,16 +137,18 @@ def plot(a, b, charges_0, charges):
     
     #Define a superfície da elipse
     x_superficie = np.linspace(-a, a, 1000) #Para gerar a superfície
-    #y_superficie = b*np.sqrt(1-(x_superficie/a)**2)
+    y_superficie = b*np.sqrt(1-(x_superficie/a)**2)
+    
+    if condicao == 1:
+        plt.plot(a+20,b+20,marker='o',color='pink')
     
     plt.axes().set_aspect('equal') #Mesmo aspect ratio para que vejamos a deformação da elipse
     plt.grid()
     plt.title("Condutor elíptico bidimensional")
     plt.xlabel('x')
     plt.ylabel('y')
-    plt.plot(x_superficie, y_superficie, color='yellow') #Superfície
-    plt.plot(x_superficie, -y_superficie, color='yellow') #Superfície
-    plt.plot(a+20,b+20,marker='o',color='pink')
+    plt.plot(x_superficie, y_superficie, color='grey') #Superfície
+    plt.plot(x_superficie, -y_superficie, color='grey') #Superfície
     plt.scatter(x_0, y_0, s=3, color='red', label = 'Inicial') #Inicial
     plt.scatter(x, y, s=3, color='darkblue', label ='Final') #Final
     plt.legend(loc=4)
@@ -138,13 +162,14 @@ a = a()
 b = b()
 n = n()
 
-condicao = 1
+condicao = 0
+potencial = 0
 
 start = time.time()
 
 charges = initial_condition_np(a,b,n)
 charges_0 = np.copy(charges)
-charges = simulate(a,b,n, charges,condicao)
+charges = simulate(a,b,n, charges,condicao,potencial)
 campo = campo_eletrico(a,b,n,charges)
 
 end = time.time()
@@ -152,4 +177,4 @@ print('Tempo de simulação: ' + str(end - start))
 
 
 #%% PLOT (LARGE STEP)
-plot(a, b, charges_0, charges)
+plot(a, b, charges_0, charges, condicao)
